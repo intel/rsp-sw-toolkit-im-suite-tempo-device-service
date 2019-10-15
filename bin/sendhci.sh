414a742436be42a76d9544ff2e662ad165346c43
alexander-j-saites <alexander.j.saites@intel.com>
2019-10-15 15:50:33 -0700

    Make scanning/hci dump logic more robust

--- bin/sendhci.sh
+++ bin/sendhci.sh
@@ -3,79 +3,133 @@
 name=$0
 function usage() {
         echo "usage: $name [OPTIONS]"
-        echo ""
         echo "OPTIONS:"
         echo "     --host|-h ENDPOINT"
         echo "         where to send hcidump data"
-        echo "         default: localhost:9001/hcidump"
+        echo "         default: localhost:80/hcidump"
         echo ""
         echo "     --device|-d HCI_DEVICE_NUM"
         echo "         number of the hci device to use"
         echo "         default: 0"
         echo ""
+        echo "     --whitelist|-w"
+        echo "         pass --whitelist option to lescan"
+        echo "         default: false"
+        echo ""
         echo "     --verbose|-v"
         echo "         print scan data while running"
         echo "         default: false"
-        echo ""
-        echo "EXAMPLE:"
-        echo "  $name --host 192.168.99.101:9001/hcidump"
 }
 
-# read args
+function ensure_arg() {
+        if [[ $# -ne 2 ]]; then
+                echo "missing arg for $1"
+                usage; exit 1
+        fi
+}
+
+hostaddr=localhost:80/hcidump
+device=0
+
+
+
+# read options
 while [[ "$1" =~ ^- ]]; do case $1 in
         --host | -h)
-                shift; hostaddr=$1
+                ensure_arg $1 $2; shift; hostaddr=$1
                 ;;
         --device | -d)
-                shift; device=$1
+                ensure_arg $1 $2; shift; device=$1
+                ;;
+        --whitelist | -w)
+                whitelist=1
                 ;;
         --verbose | -v)
                 verbose=1
                 ;;
         *)
-                usage
-                exit 1
+                usage; exit 1
                 ;;
 esac; shift; done
 
-# report extra args
-if [[ $@ ]]; then
-        usage
-        exit 1
+# consider it an error if there were more options supplied
+if [[ $# -ne 0 ]]; then
+        echo 'too many args'
+        usage; exit 1
 fi
 
-# start BLE scanning
+function check_device() {
+        if [[ "$1" != "$device" ]]; then
+                return
+        fi
+
+        found=true
+        if [[ "$2" != "bluetooth" ]]; then
+                echo "device $1 is $2, not bluetooth"
+                exit 1;
+        fi
+
+        # check softblocks
+        if [[ "$3" != "unblocked" ]]; then
+                echo "removing softblock from $1..."
+                (set -x && rfkill unblock bluetooth)
+                if [[ $(rfkill list 0 --noheading --output SOFT) != "unblocked" ]]; then
+                        echo "device $1 is '$3', but 'rfkill unblock bluetooth' failed"
+                        exit 1;
+                fi
+        fi
+
+        # check hardblocks
+        if [[ "$4" != "unblocked" ]]; then
+                echo "device $1 is $4"
+                exit 1;
+        fi
+}
+
+# find_bluetooth goes through the devices looking for this one
+function find_bluetooth() {
+        found=false
+        while read -r line; do
+                check_device $line
+                if [[ $found == "true" ]]; then
+                        break;
+                fi
+        done <<< "$(rfkill list --noheading --output ID,TYPE,SOFT,HARD)"
+        if [[ $found != "true" ]]; then
+                echo "cannot find device $device";
+                exit 1;
+        fi
+}
+
+find_bluetooth
+
+scan_opts='--duplicates'
+if [[ $whitelist ]]; then
+        scan_opts="$scan_opts --whitelist"
+fi
+
+# start scanning
 if [[ $verbose ]]; then
-        hcitool lescan --duplicates &
+        xargs_opt='-t'
+        (set -x && hcitool lescan $scan_opts) &
 else
-        hcitool lescan --duplicates 1> /dev/null &
+        xargs_opt=''
+        hcitool lescan $scan_opts 1> /dev/null &
 fi
 
-# send hcidump data to the data processor:
-#   hcidump (get the data):
-#     -i specifies device ID
-#     -R dumps "raw" hex data
-#   xargs (create a command for each piece of data):
-#     -d splits the hcidump data on '>' (it still has newlines)
-#     -I{} replaces '{}' with the hcidump data in the curl command
-#   curl (send the data upstream):
-#     -s -S is silent except for errors
-#     -H sets a header - we're sending text
-#     -X specifies the HTTP verb (POST)
-#     -d gives the data, which is replaced with the hex dump
-#     the final arg is the host address
+# grab HCI data and send it to the processor
 (hcidump -i ${device:-0} -R | \
-    xargs -d '>' -I{} \
+    xargs $xargs_opt -d '>' -I{} \
         curl \
         -s -S \
         -H "Content-Type: application/text" \
         -X POST \
         -d '{}' \
-        ${hostaddr:-localhost:9001/hcidump}) &
+        ${hostaddr:-localhost:80/hcidump}) &
 
-# wait for signal/shutdown
+# trap ctrl+c; kill the processes
 trap "trap - SIGTERM && kill -- -$$ 2> /dev/null" SIGINT SIGTERM EXIT
-echo "started $(jobs -pr); use ctrl+c to stop"
+echo "started $(jobs -pr | tr '\n' ' '); use SIGTERM (usually ctrl+c) to stop"
 echo "sending data from hci${device:-0} to ${hostaddr:-localhost:80/hcidump}"
 wait
 echo "stopped"
